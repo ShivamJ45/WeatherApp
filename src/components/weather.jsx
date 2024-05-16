577e30e63f7200dc0de5461fa3ffac1f3b4bbf12
ShivamJ45 <shivamajadhav@gmail.com>
2024-05-17 02:14:33 +0530

    Initial Commit

---
+++ src/components/weather.jsx
@@ -0,0 +1,15 @@
+import "./weather.css"
+
+const weather = () => {
+    return(
+        <>
+            <div className="mainDiv">
+                <div className="city">
+                    <input type="text" placeholder="Enter any city name" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default weather;
